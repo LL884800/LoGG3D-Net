6477a76e5e764a3177d1f28482bc13b1684a64ea
LL884800 <152848653+LL884800@users.noreply.github.com>
2024-11-19 19:17:38 +0800

    spvcnn.py

--- models/backbones/spvnas/core/models/semantic_kitti/spvcnn.py
+++ models/backbones/spvnas/core/models/semantic_kitti/spvcnn.py
@@ -11,7 +11,7 @@ from core.models.utils import *
 
 __all__ = ['SPVCNN']
 
-#个基本的稀疏卷积模块，由 3D 卷积、批归一化（BatchNorm）和激活函数（ReLU）组成。支持自定义卷积核大小、步幅和膨胀率
+#基本的稀疏卷积模块，由 3D 卷积、批归一化（BatchNorm）和激活函数（ReLU）组成。支持自定义卷积核大小、步幅和膨胀率
 #BasicDeconvolutionBlock：用于上采样（反卷积）的模块，与卷积块类似，但使用了转置卷积（transposed convolution）实现
 class BasicConvolutionBlock(nn.Module):
     def __init__(self, inc, outc, ks=3, stride=1, dilation=1):
@@ -27,8 +27,7 @@ class BasicConvolutionBlock(nn.Module):
     def forward(self, x):
         out = self.net(x)
         return out
-
-
+#基本反卷积块，上采样层，用于对下采样后的特征图进行恢复。通过反卷积，可以将较低分辨率的特征图恢复为更高分辨率的特征图
 class BasicDeconvolutionBlock(nn.Module):
     def __init__(self, inc, outc, ks=3, stride=1):
         super().__init__()
@@ -43,7 +42,7 @@ class BasicDeconvolutionBlock(nn.Module):
     def forward(self, x):
         return self.net(x)
 
-
+#残差块
 class ResidualBlock(nn.Module):
     def __init__(self, inc, outc, ks=3, stride=1, dilation=1):
         super().__init__()
@@ -72,25 +71,26 @@ class ResidualBlock(nn.Module):
         out = self.relu(self.net(x) + self.downsample(x))
         return out
 
-
+#SPVCNN 是一个用于点云分类的深度神经网络
 class SPVCNN(nn.Module):
     def __init__(self, **kwargs):
+        #SPVCNN 初始化时根据传入的参数设置各层的卷积和反卷积操作。网络包含了一个逐步提取特征的过程，并在每个阶段使用卷积和残差模块
         super().__init__()
 
-        cr = kwargs.get('cr', 1.0)
-        cs = [32, 32, 64, 128, 256, 256, 128, 96, 96]
+        cr = kwargs.get('cr', 1.0) # 通道缩放因子
+        cs = [32, 32, 64, 128, 256, 256, 128, 96, 96] # 根据缩放因子调整每一层的通道数
         cs = [int(cr * x) for x in cs]
-
+        # 点云数据体素化后的分辨率
         if 'pres' in kwargs and 'vres' in kwargs:
             self.pres = kwargs['pres']
             self.vres = kwargs['vres']
-
+        # 初始卷积层（stem）
         self.stem = nn.Sequential(
             spnn.Conv3d(4, cs[0], kernel_size=3, stride=1),
             spnn.BatchNorm(cs[0]), spnn.ReLU(True),
             spnn.Conv3d(cs[0], cs[0], kernel_size=3, stride=1),
             spnn.BatchNorm(cs[0]), spnn.ReLU(True))
-
+         # 各个阶段（stage）的卷积和残差模块
         self.stage1 = nn.Sequential(
             BasicConvolutionBlock(cs[0], cs[0], ks=2, stride=2, dilation=1),
             ResidualBlock(cs[0], cs[1], ks=3, stride=1, dilation=1),
@@ -114,7 +114,7 @@ class SPVCNN(nn.Module):
             ResidualBlock(cs[3], cs[4], ks=3, stride=1, dilation=1),
             ResidualBlock(cs[4], cs[4], ks=3, stride=1, dilation=1),
         )
-
+         # 上采样模块（up1, up2, up3, up4）
         self.up1 = nn.ModuleList([
             BasicDeconvolutionBlock(cs[4], cs[5], ks=2, stride=2),
             nn.Sequential(
@@ -150,10 +150,10 @@ class SPVCNN(nn.Module):
                 ResidualBlock(cs[8], cs[8], ks=3, stride=1, dilation=1),
             )
         ])
-
+         # 分类器（输出类别）
         self.classifier = nn.Sequential(nn.Linear(cs[8],
                                                   kwargs['num_classes']))
-
+        # 点云特征变换模块
         self.point_transforms = nn.ModuleList([
             nn.Sequential(
                 nn.Linear(cs[0], cs[4]),
@@ -171,8 +171,10 @@ class SPVCNN(nn.Module):
                 nn.ReLU(True),
             )
         ])
-
+        
+         # 权重初始化
         self.weight_initialization()
+         # Dropout层
         self.dropout = nn.Dropout(0.3, True)
 
     def weight_initialization(self):
@@ -181,14 +183,14 @@ class SPVCNN(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-    def forward(self, x): #点云初始化，输入是稀疏张量，将其转换成点张量
-        # x: SparseTensor z: PointTensor
-        z = PointTensor(x.F, x.C.float())
+    def forward(self, x): #点云初始化，输入是稀疏张量，将其转换成点张量  前向传播过程，对输入的稀疏点云数据进行处理，逐步提取特征并完成分类任务
+        # x: SparseTensor z: PointTensor  #x是稀疏张量，包含点云数据
+        z = PointTensor(x.F, x.C.float()) # x.F 表示点云的特征（例如点云的属性，如强度、颜色等）
 
         x0 = initial_voxelize(z, self.pres, self.vres) #将点云数据转换成稀疏体素
 
-        x0 = self.stem(x0)
-        z0 = voxel_to_point(x0, z, nearest=False)
+        x0 = self.stem(x0) #对体素数据进行特征提取
+        z0 = voxel_to_point(x0, z, nearest=False) #体素数据转化为点云数据，便于后续操作
         z0.F = z0.F
     #提取特征，同时逐步下采样
         x1 = point_to_voxel(x0, z0)
