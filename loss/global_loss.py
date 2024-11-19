6eab0a724975a1138644c879608343910fb5d93d
LL884800 <152848653+LL884800@users.noreply.github.com>
2024-11-19 14:05:46 +0800

    global_loss.py

--- loss/global_loss.py
+++ loss/global_loss.py
@@ -2,19 +2,20 @@
 import numpy as np
 import torch
 
-
+#函数用于计算查询样本（query）与正样本集（pos_vecs）之间的最大距离
 def best_pos_distance(query, pos_vecs):
     num_pos = pos_vecs.shape[0]
-    query_copies = query.repeat(int(num_pos), 1)
+    query_copies = query.repeat(int(num_pos), 1) #查询向量 query 进行复制，使其与正样本集 pos_vecs 的每个样本进行配对
     # ((pos_vecs - query_copies) ** 2).sum(2)
-    diff = ((pos_vecs - query_copies)**2).sum(1)
+    diff = ((pos_vecs - query_copies)**2).sum(1)  #每个正样本与查询样本之间的平方欧氏距离
     max_pos_dist = diff.max()
-    return max_pos_dist
+    return max_pos_dist #返回正样本中与查询样本的最大距离
 
-
+#三元组损失用于训练模型
 def triplet_loss(tuple_vecs, config):
     tuple_vecs = torch.split(
         tuple_vecs, [1, config.positives_per_query, config.negatives_per_query])
+    #查询向量（q_vec）、正样本向量（pos_vecs）和负样本向量（neg_vecs）
     q_vec, pos_vecs, neg_vecs = tuple_vecs[0], tuple_vecs[1], tuple_vecs[2]
     max_pos_dist = best_pos_distance(q_vec, pos_vecs)
 
@@ -26,7 +27,10 @@ def triplet_loss(tuple_vecs, config):
     neg_dists = ((neg_vecs - query_copies) ** 2).sum(1)
     loss = config.loss_margin_1 + max_pos_dist - \
         neg_dists.reshape((int(num_neg), 1))
+    #计算损失 loss，它是查询样本与负样本之间距离与最大正样本距离之差。然后对损失进行裁剪，确保其最小值为0
+    
     loss = loss.clamp(min=0.0)
+    #如果配置 lazy_loss 为 True，则损失取最大值；否则，损失取所有负样本损失的和
     if config.lazy_loss:
         triplet_loss = loss.max()
     else:
@@ -34,11 +38,12 @@ def triplet_loss(tuple_vecs, config):
 
     return triplet_loss
 
-
+#四元组损失是三元组损失的扩展，除了正样本和负样本外，还考虑了额外的负样本other_neg
 def quadruplet_loss(tuple_vecs, config):
     tuple_vecs = torch.split(
         tuple_vecs, [1, config.positives_per_query, config.negatives_per_query, 1])
     q_vec, pos_vecs, neg_vecs, other_neg = tuple_vecs[0], tuple_vecs[1], tuple_vecs[2], tuple_vecs[3]
+    #输入的 tuple_vecs 包含查询向量（q_vec）、正样本向量（pos_vecs）、负样本向量（neg_vecs）和其他负样本向量（other_neg）
     max_pos_dist = best_pos_distance(q_vec, pos_vecs)
 
     num_neg = neg_vecs.shape[0]
@@ -49,7 +54,8 @@ def quadruplet_loss(tuple_vecs, config):
     neg_dists = ((neg_vecs - query_copies) ** 2).sum(1)
     loss = config.loss_margin_1 + max_pos_dist - \
         neg_dists.reshape((int(num_neg), 1))
-    loss = loss.clamp(min=0.0)
+    loss = loss.clamp(min=0.0) #clamp(min=0.0)：这是为了确保损失不为负值，因为我们希望最小损失为0，表示正样本和负样本的距离已经足够分开
+    #如果配置 lazy_loss 为 True，则第二个损失取最大值；否则，取所有第二负样本损失的和。
     if config.lazy_loss:
         triplet_loss = loss.max()
     else:
